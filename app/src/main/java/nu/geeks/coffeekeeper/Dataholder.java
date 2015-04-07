886a7c562c4495ce0b4e5b7ad3841b37f8ae6c95
ramonnr <ramonnr@kth.se>
2015-04-07 17:09:01 +0200

    update dataholder

--- app/src/main/java/nu/geeks/coffeekeeper/Dataholder.java
+++ app/src/main/java/nu/geeks/coffeekeeper/Dataholder.java
@@ -1,7 +1,7 @@
 package nu.geeks.coffeekeeper;
 
 /**
- * Created by dudez on 2015-04-07.
+ * Created by ramonicon
  */
 import java.util.ArrayList;
 
@@ -11,7 +11,7 @@ public class Dataholder {
     private int grind;
     private int temp;
     private ArrayList<Integer> brewTime;
-    private int totalTime;
+    //private int totalTime;
     private String kindCoffe;
     private String comments;
     private String name;
@@ -23,7 +23,7 @@ public class Dataholder {
         grind=0;
         temp=0;
         brewTime=new ArrayList<Integer>();
-        totalTime=0;
+      //  totalTime=0;
         kindCoffe="";
         comments="";
         name="";
@@ -36,14 +36,13 @@ public class Dataholder {
         grind=0;
         temp=0;
         brewTime=new ArrayList<Integer>();
-        totalTime=0;
+       // totalTime=0;
         kindCoffe="";
         comments="";
         this.name=name;
     }
 
     //set functions
-
     public void setAmountCoffe(int amountCoffe) {
         this.amountCoffe=amountCoffe;
     }
@@ -61,18 +60,108 @@ public class Dataholder {
     }
 
     public void setBrewTime(int index, int brewTime){
-        if(index>this.brewTime.size()){
+        //if index is larger than size, then just add element
+        if(index>this.brewTime.size()) {
             this.brewTime.add(brewTime);
-        }else{
+        }else{      //else remove previous entry then add current element
             this.brewTime.remove(index);
             this.brewTime.add(index,brewTime);
         }
     }
 
-    public void setTotalTime(){
-    
+    public void setKindCoffe(String kindCoffe){
+        this.kindCoffe=new String(kindCoffe);
+    }
+
+    public void setComments(String comments){
+        this.comments=new String(comments);
+    }
+
+    public void setName(String name){
+        this.name=new String(name);
+    }
+
+    //get functions
+    public int getAmountCoffe(){
+        return amountCoffe;
+    }
+
+    public int getAmountWater(){
+        return amountWater;
+    }
+
+    public int getGrind(){
+        return grind;
+    }
+
+    public int getTemp(){
+        return temp;
+    }
+
+    public ArrayList<Integer> getBrewTime(){
+        return new ArrayList<Integer>(brewTime);
+    }
+
+    public int getTotalTime(){
+        int totTime=0;
+        for(Integer x: brewTime){
+            totTime+=x;
+        }
+        return totTime;
+    }
+
+    public String getKindCoffe(){
+        return new String(kindCoffe);
+    }
+
+    public String getComments(){
+        return new String(comments);
+    }
+
+    public String getName(){
+        return new String(name);
     }
 
 
 
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
